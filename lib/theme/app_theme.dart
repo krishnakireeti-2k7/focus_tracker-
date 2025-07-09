3734689e5fefdb8cc989f3102b5ee9e15d41ae82
Krishna Kireeti <krishnakireeti2k7@gmail.com>
2025-07-09 10:02:08 +0530

    theme Data

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    scaffoldBackgroundColor: Colors.white,
+    colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.blue
+    ),
     useMaterial3: true,
   );
 
